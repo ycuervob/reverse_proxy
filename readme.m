bf22373afa0ef805a7165fc9da69fb5bbe17127e
Jahel_Leon <jleonro@unal.edu.co>
2022-11-09 10:49:07 -0500

    added docker internal

--- readme.m
+++ readme.m
@@ -1,3 +1,4 @@
 docker build -t jleonro/reverse_proxy .
 
-docker run -p 80:80 -d --name proxy reverse-proxy 
+docker run -p 80:80 --add-host=host.docker.internal:host-gateway -d --name proxy reverse-proxy 
+
